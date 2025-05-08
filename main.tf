b8173deadaee1a529a8b43151f357b7f07782945
Omo Elias <Obedelias01@gmail.com>
2025-05-08 08:32:39 +0200

    create a target group

--- main.tf
+++ main.tf
@@ -232,25 +232,25 @@ resource "aws_lb" "my_alb" {
   }
 }
 # # Create Target Group for ALB
-# resource "aws_lb_target_group" "my_target_group" {
-#   name     = "my-target-group"
-#   port     = 80
-#   protocol = "HTTP"
-#   vpc_id   = aws_vpc.my_vpc.id
-#   health_check {
-#     path                = "/"
-#     interval            = 30
-#     timeout             = 5
-#     healthy_threshold  = 2
-#     unhealthy_threshold = 2
-#     matcher {
-#       http_code = "200,302"
-#     }
-#   }
-#   tags = {
-#     Name = "My_Target_Group"
-#   }
-# }
+resource "aws_lb_target_group" "my_target_group" {
+  name     = "my-target-group"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.my_vpc.id
+  health_check {
+    path                = "/"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold  = 2
+    unhealthy_threshold = 2
+    matcher {
+      http_code = "200,302"
+    }
+  }
+  tags = {
+    Name = "My_Target_Group"
+  }
+}
 # # Create Listener for ALB
 # resource "aws_lb_listener" "my_listener" {
 #   load_balancer_arn = aws_lb.my_alb.arn
