91ff0756e8f47557fdb9c6365c5731348c91de00
Omo Elias <Obedelias01@gmail.com>
2025-06-20 21:25:34 +0200

    modification

--- main.tf
+++ main.tf
@@ -216,41 +216,41 @@ resource "aws_security_group" "alb_sg" {
   }
 }
 # Create Application Load Balancer  
-resource "aws_lb" "my_alb" {
-  name               = "my-alb"
-  internal           = false
-  load_balancer_type = "application"
-  security_groups    = [aws_security_group.alb_sg.id]
-  subnets            = [aws_subnet.public_subnet_1.id, aws_subnet.public_subnet_2.id, aws_subnet.public_subnet_3.id]
-  enable_deletion_protection = false
-  enable_http2 = true
-  idle_timeout {
-    timeout_seconds = 60
-  }
-  tags = {
-    Name = "My_ALB"
-  }
-}
+# resource "aws_lb" "my_alb" {
+#   name               = "my-alb"
+#   internal           = false
+#   load_balancer_type = "application"
+#   security_groups    = [aws_security_group.alb_sg.id]
+#   subnets            = [aws_subnet.public_subnet_1.id, aws_subnet.public_subnet_2.id, aws_subnet.public_subnet_3.id]
+#   enable_deletion_protection = false
+#   enable_http2 = true
+#   idle_timeout {
+#     timeout_seconds = 60
+#   }
+#   tags = {
+#     Name = "My_ALB"
+#   }
+# }
 # # Create Target Group for ALB
-resource "aws_lb_target_group" "my_target_group" {
-  name     = "my-target-group"
-  port     = 80
-  protocol = "HTTP"
-  vpc_id   = aws_vpc.my_vpc.id
-  health_check {
-    path                = "/"
-    interval            = 30
-    timeout             = 5
-    healthy_threshold  = 2
-    unhealthy_threshold = 2
-    matcher {
-      http_code = "200,302"
-    }
-  }
-  tags = {
-    Name = "My_Target_Group"
-  }
-}
+# resource "aws_lb_target_group" "my_target_group" {
+#   name     = "my-target-group"
+#   port     = 80
+#   protocol = "HTTP"
+#   vpc_id   = aws_vpc.my_vpc.id
+#   health_check {
+#     path                = "/"
+#     interval            = 30
+#     timeout             = 5
+#     healthy_threshold  = 2
+#     unhealthy_threshold = 2
+#     matcher {
+#       http_code = "200,302"
+#     }
+#   }
+#   tags = {
+#     Name = "My_Target_Group"
+#   }
+# }
 # # Create Listener for ALB
 # resource "aws_lb_listener" "my_listener" {
 #   load_balancer_arn = aws_lb.my_alb.arn
